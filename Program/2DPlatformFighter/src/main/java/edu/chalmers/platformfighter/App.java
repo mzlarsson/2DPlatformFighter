2d145aa3b77d2c374a0c694daa78d954da432270
Patrik Haar <haar@student.chalmers.se>
2014-03-21 00:01:14 +0100

    The LWJGL natives is now imported as they should but to run in eclipse you need to add '-Djava.library.path=target/natives' as a VM argument in the Run configuration.

--- Program/2DPlatformFighter/src/main/java/edu/chalmers/platformfighter/App.java
+++ Program/2DPlatformFighter/src/main/java/edu/chalmers/platformfighter/App.java
@@ -1,6 +1,8 @@
 package edu.chalmers.platformfighter;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
@@ -9,6 +11,12 @@ import org.newdawn.slick.SlickException;
 
 public class App extends BasicGame
 {
+	double origPosY = 200;
+	double posY = 200;
+	double yGrav = 0.0005;
+	double speed = 0;
+	Date timer = new Date();	
+	long airtime = timer.getTime();
 	public App(String gamename)
 	{
 		super(gamename);
@@ -20,9 +28,20 @@ public class App extends BasicGame
 	@Override
 	public void update(GameContainer gc, int i) throws SlickException {}
 
-	public void render(GameContainer gc, Graphics g) throws SlickException
-	{
-		g.drawString("Howdy!", 300, 300);
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+//		System.out.println((timer.getTime()-airtime));
+//		System.out.println(timer.getTime());
+//		posY = origPosY + (speed+yGrav*(timer.getTime()-airtime))*(timer.getTime()-airtime);
+		speed += yGrav;
+		posY += speed;
+		if (posY>=480) {
+			System.out.println("Boing!");
+			posY = 480;
+			origPosY = 480;
+			airtime = timer.getTime();
+			speed = -0.5;
+		}
+		g.drawString("Howdy!", 300, (int)posY);
 	}
 
 	public static void main(String[] args)
