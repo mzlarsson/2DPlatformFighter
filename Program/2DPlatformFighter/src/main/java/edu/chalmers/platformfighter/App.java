a14453873dd9f37dd6a175b7dd7c27c83f5a9c57
Patrik Haar <haar@student.chalmers.se>
2014-03-20 18:21:55 +0100

    Hopefully made the natives for the LWJGL load correctly

--- Program/2DPlatformFighter/src/main/java/edu/chalmers/platformfighter/App.java
+++ Program/2DPlatformFighter/src/main/java/edu/chalmers/platformfighter/App.java
@@ -1,13 +1,42 @@
 package edu.chalmers.platformfighter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App extends BasicGame
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public App(String gamename)
+	{
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.drawString("Howdy!", 300, 300);
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new App("Simple Slick Game"));
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
