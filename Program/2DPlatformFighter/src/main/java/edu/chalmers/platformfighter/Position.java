a43a3a6207803532f99a7584c59c28f2dfba5ec8
Patrik Haar <haar@student.chalmers.se>
2014-03-28 21:26:44 +0100

    Added empty classes for Position and Velocity

---
+++ Program/2DPlatformFighter/src/main/java/edu/chalmers/platformfighter/Position.java
@@ -0,0 +1,25 @@
+package edu.chalmers.platformfighter;
+
+public class Position {
+
+	public Position(float x, float y) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Position copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public float getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public float getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+}
---
+++ Program/2DPlatformFighter/src/main/java/edu/chalmers/platformfighter/Velocity.java
@@ -0,0 +1,30 @@
+package edu.chalmers.platformfighter;
+
+public class Velocity {
+
+	public Velocity(float x, float y) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Velocity copy() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	//TODO This should be in the abstract Vector class.
+	public Velocity add(Velocity variableSpeed) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public float getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public float getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
