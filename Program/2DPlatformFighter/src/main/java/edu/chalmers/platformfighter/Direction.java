d85547fbf5e1e37d9763d3a5bb4af9ab8d16c4ff
Matz Larsson <matzl@student.chalmers.se>
2014-03-31 23:47:45 +0200

    Added simple Direction enum class for movement handling

---
+++ Program/2DPlatformFighter/src/main/java/edu/chalmers/platformfighter/Direction.java
@@ -0,0 +1,14 @@
+package edu.chalmers.platformfighter;
+
+/**
+ * Enum for holding simple movement directions.
+ * 
+ * @author Matz Larsson
+ * @version 0.1
+ *
+ */
+
+public enum Direction {
+	//All values are self explaining.
+	LEFT, RIGHT, UP, DOWN, NONE
+}
