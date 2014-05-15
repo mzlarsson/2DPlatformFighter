e7b2d4f9fc131e83d53c86329dd37c13a62ed284
Patrik Haar <haar@student.chalmers.se>
2014-05-15 18:09:15 +0200

    Changed so you no longer have to add VM Arguments to the run configuration

--- Program/2DPlatformFighter/src/main/java/edu/chalmers/brawlbuddies/Main.java
+++ Program/2DPlatformFighter/src/main/java/edu/chalmers/brawlbuddies/Main.java
@@ -1,6 +1,11 @@
 package edu.chalmers.brawlbuddies;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
+import java.net.URLDecoder;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import junit.framework.Test;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
@@ -17,11 +22,32 @@ import edu.chalmers.brawlbuddies.controller.Controller;
 public class Main {
 
 	/**
+	 * Sets the java library path to the specified path
+	 *
+	 * @param path the new library path
+	 * @throws Exception
+	 */
+	public static void setLibraryPath(String path) throws Exception {
+	    System.setProperty("java.library.path", path);
+	 
+	    //set sys_paths to null
+	    final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
+	    sysPathsField.setAccessible(true);
+	    sysPathsField.set(null, null);
+	}
+	/**
 	 * Creates a controller and starts a game.
 	 * @param args	Parameters in.
 	 * 				NOTE: This variable is not used.
 	 */
 	public static void main(String[] args){
+		try {
+			setLibraryPath("target/natives");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(System.getProperty("java.library.path"));
 		Controller controller = new Controller();
 		try {
 			AppGameContainer appgc;
